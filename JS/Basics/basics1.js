a5cf6dd5457c9c6e8c989b2371ba195ff8f23230
inkollusireeshaadharani <86912049+inkollusireeshaadharani@users.noreply.github.com>
2022-03-15 06:04:15 +0530

    Add files via upload

---
+++ JS/Basics/basics1.js
@@ -0,0 +1,11 @@
+// Write a program which consist of single line and multiline comments.
+var a=1;
+//this is a single line comment
+var b=20;
+var c=30;
+document.write(a,b,c);
+/* these
+ are 
+ multiline
+ comments
+ */
